97cea0bbf32395d73801211058969cdda252373c
Brian Aston <astonb.dev@gmail.com>
2022-02-08 12:34:58 -0800

    Finished form templates for new user

--- app.py
+++ app.py
@@ -1,24 +1,29 @@
+import os
+
 from flask import Flask, render_template, redirect
 from flask_debugtoolbar import DebugToolbarExtension
 from models import connect_db, db, User
-from forms import RegisterUserForm, LoginForm, CSRFProtectForm
+from forms import RegisterUserForm
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///hashing_login"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
-app.config["SECRET_KEY"] = "abc123"
+app.config["SECRET_KEY"] = os.environ["API_SECRET_KEY"]
 
 connect_db(app)
 db.create_all()
 
 toolbar = DebugToolbarExtension(app)
 
+
 @app.get('/')
 def show_homepage():
+
     return redirect('/register')
 
-@app.route('/register', method=["GET", "POST"])
+
+@app.route('/register', methods=["GET", "POST"])
 def register_new_user():
     form = RegisterUserForm()
 
@@ -29,7 +34,8 @@ def register_new_user():
         first_name = form.first_name.data
         last_name = form.last_name.data
 
-        new_user = User.register(username, password, email, first_name, last_name)
+        new_user = User.register(
+            username, password, email, first_name, last_name)
 
         db.session.add(new_user)
         db.session.commit()
--- forms.py
+++ forms.py
@@ -2,15 +2,16 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, TextAreaField, BooleanField, PasswordField
 from wtforms.validators import InputRequired, Length, URL, Optional
 
+
 class RegisterUserForm(FlaskForm):
     """Register User Form class"""
     username = StringField('User Name',
                            validators=[InputRequired()])
     password = PasswordField('Password',
-                           validators=[InputRequired()])
+                             validators=[InputRequired()])
     email = StringField('Email Address',
-                           validators=[InputRequired()])
+                        validators=[InputRequired()])
     first_name = StringField('First Name',
-                           validators=[InputRequired()])
+                             validators=[InputRequired()])
     last_name = StringField('Last Name',
-                           validators=[InputRequired()])
+                            validators=[InputRequired()])
